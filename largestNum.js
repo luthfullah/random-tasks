85b623d79baea800d20b9a6b655dd70bceac625e
luthfullah <luthfullah111@gmail.com>
2024-10-04 22:39:40 +0500

    subset values

--- largestNum.js
+++ largestNum.js
@@ -1,3 +1,9 @@
+
+
+
+// ****** Assignment # 1 **** // 
+// ****** Name: Lutfullah *** //
+
 const arr = [10, 2, 20, 4, 30, 6, 8, 9];
 let highestValue = arr[0];
 for (let i = 0; i < arr.length; i++) {
@@ -6,3 +12,4 @@ for (let i = 0; i < arr.length; i++) {
   }
 }
 console.log("luthfi your highest value is : ",highestValue);
+//output: luthfi your highest value is :  30
---
+++ subsetValues.js
@@ -0,0 +1,80 @@
+function subset(arr){
+    for(let i=0;i < arr.length - 2;i++){
+        for(let j=1;j<arr.length -1;j++){
+            for(let k=2;k<arr.length ; k++){
+                console.log("Luthfi your subset values are : [",arr[i], " ", arr[j]," ",arr[k]," ]")
+            }
+        }
+    }
+
+}
+subset([1,2,3,4,5,6])
+
+// output:
+// Luthfi your subset values are : [ 1   2   3  ]
+// Luthfi your subset values are : [ 1   2   4  ]
+// Luthfi your subset values are : [ 1   2   5  ]
+// Luthfi your subset values are : [ 1   2   6  ]
+// Luthfi your subset values are : [ 1   3   3  ]
+// Luthfi your subset values are : [ 1   3   4  ]
+// Luthfi your subset values are : [ 1   3   5  ]
+// Luthfi your subset values are : [ 1   3   6  ]
+// Luthfi your subset values are : [ 1   4   3  ]
+// Luthfi your subset values are : [ 1   4   4  ]
+// Luthfi your subset values are : [ 1   4   5  ]
+// Luthfi your subset values are : [ 1   4   6  ]
+// Luthfi your subset values are : [ 1   5   3  ]
+// Luthfi your subset values are : [ 1   5   4  ]
+// Luthfi your subset values are : [ 1   5   5  ]
+// Luthfi your subset values are : [ 1   5   6  ]
+// Luthfi your subset values are : [ 2   2   3  ]
+// Luthfi your subset values are : [ 2   2   4  ]
+// Luthfi your subset values are : [ 2   2   5  ]
+// Luthfi your subset values are : [ 2   2   6  ]
+// Luthfi your subset values are : [ 2   3   3  ]
+// Luthfi your subset values are : [ 2   3   4  ]
+// Luthfi your subset values are : [ 2   3   5  ]
+// Luthfi your subset values are : [ 2   3   6  ]
+// Luthfi your subset values are : [ 2   4   3  ]
+// Luthfi your subset values are : [ 2   4   4  ]
+// Luthfi your subset values are : [ 2   4   5  ]
+// Luthfi your subset values are : [ 2   4   6  ]
+// Luthfi your subset values are : [ 2   5   3  ]
+// Luthfi your subset values are : [ 2   5   4  ]
+// Luthfi your subset values are : [ 2   5   5  ]
+// Luthfi your subset values are : [ 2   5   6  ]
+// Luthfi your subset values are : [ 3   2   3  ]
+// Luthfi your subset values are : [ 3   2   4  ]
+// Luthfi your subset values are : [ 3   2   5  ]
+// Luthfi your subset values are : [ 3   2   6  ]
+// Luthfi your subset values are : [ 3   3   3  ]
+// Luthfi your subset values are : [ 3   3   4  ]
+// Luthfi your subset values are : [ 3   3   5  ]
+// Luthfi your subset values are : [ 3   3   6  ]
+// Luthfi your subset values are : [ 3   4   3  ]
+// Luthfi your subset values are : [ 3   4   4  ]
+// Luthfi your subset values are : [ 3   4   5  ]
+// Luthfi your subset values are : [ 3   4   6  ]
+// Luthfi your subset values are : [ 3   5   3  ]
+// Luthfi your subset values are : [ 3   5   4  ]
+// Luthfi your subset values are : [ 3   5   5  ]
+// Luthfi your subset values are : [ 3   5   6  ]
+// Luthfi your subset values are : [ 4   2   3  ]
+// Luthfi your subset values are : [ 4   2   4  ]
+// Luthfi your subset values are : [ 4   2   5  ]
+// Luthfi your subset values are : [ 4   2   6  ]
+// Luthfi your subset values are : [ 4   3   3  ]
+// Luthfi your subset values are : [ 4   3   4  ]
+// Luthfi your subset values are : [ 4   3   5  ]
+// Luthfi your subset values are : [ 4   3   6  ]
+// Luthfi your subset values are : [ 4   4   3  ]
+// Luthfi your subset values are : [ 4   4   4  ]
+// Luthfi your subset values are : [ 4   4   5  ]
+// Luthfi your subset values are : [ 4   4   6  ]
+// Luthfi your subset values are : [ 4   5   3  ]
+// Luthfi your subset values are : [ 4   5   4  ]
+// Luthfi your subset values are : [ 4   5   5  ]
+// Luthfi your subset values are : [ 4   5   6  ]
+
+
+
